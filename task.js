3aff0dcd460c50aa771a15da49747ad8f86a4c77
Leshon Johnson-Hampton <leshonhampton@yahoo.com>
2021-10-19 11:21:19 -0400

    Set up feature where the menu will continue to pop up if something other than CLOSE is entered.

--- task.js
+++ task.js
@@ -12,3 +12,14 @@ What would you like to do (Please enter one of the options below):
 `;
 // Displays the menu for the user to select an option. Also, sets the user's response to the userInput variable
 let userInput = prompt(menu);
+
+// Loops and continues to display the menu until the user ends the program (ALA until the user enters CLOSE)
+while (userInput !== `CLOSE`){
+    // OTHER OPTIONS
+
+    // Displays the menu again
+    userInput = prompt(menu);
+}
+
+//Alerts the user that they have close the program
+alert(`Thank you for using Task Manager`);
