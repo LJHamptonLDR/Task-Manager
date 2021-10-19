d6530b58f0deefb2033fd2f1e984c8e9b293e1eb
Leshon Johnson-Hampton <leshonhampton@yahoo.com>
2021-10-19 15:18:15 -0400

    The removing a task feature is complete

--- task.js
+++ task.js
@@ -4,63 +4,95 @@
 const menu = `TASK MANAGER
 
 What would you like to do (Please enter one of the options below):
-"TASKS" - Display All Task
+"TASKS" - Display All Tasks
 "NEW - Add A New Task
-"REMOVE - Remove A Task
+"REMOVE" - Remove A Task
 "CLOSE" - Close The Task Manager
 `;
 
-// Array for storing taks
+// Array for storing tasks
 const tasks = [
-    `Charge MacBook`,
-    `Master JavaScript`
+  `Charge MacBook`,
+  `Master JavaScript`
 ];
 
-// For Displaying tasks to the user
+// For displaying tasks to the user
 let showTasks = ``;
 
 // For storing the value of a new task
-let newTask; 
+let newTask;
+
+// For storing the number of the task to remove
+let num;
+
+// For storing the value of the removed task
+let removed;
 
 // Displays the menu for the user to select an option. Also, sets the user's response to the userInput variable
 let userInput = prompt(menu);
 
-// Loops and continues to display the menu until the user ends/closes the Task Manager (AKA until the user enters CLOSE)
-while (userInput !== `CLOSE`) {
+// Loops and continues to display the menu until the user ends/closes the Task Manager (AKA Until the user enters CLOSE)
+while (userInput !== `CLOSE`){
 
-    // Checks if user entered TASKS
-    if (userInput === `TASKS`) {
+  // Checks if user entered TASKS
+  if (userInput === `TASKS`){
 
-        // The For Of loop is used here to concatenate each task (AKA Item) in the tasks array to the showTasks string variable
-        for (task of tasks) {
+    // The For Of loop is used here to concatenate each task (AKA Item) in the tasks array to the showTasks string variable
+    for (task of tasks){
 
-            //Using \n to create a new line (AKA Line break after each task)
-            showTasks += `${tasks}\n`;
-        }
-
-        // Display the current tasks to the user
-        alert(showTasks);
-        
-        // Set the value of the showTasks string variable back to an empty string
-        showTasks = "";
+      // Using \n to create a new line (AKA Line break) after each task
+      showTasks += `${task}\n`;
     }
 
-    // Checks if the user entered NEW
-    if (userInput === `NEW`){
+    // Display the current tasks to the user
+    alert(showTasks);
 
-        // Prompts the user to enter a new task and stories their response
-        newTask = prompt(`Please enter the new task`);
+    // Set the value of the showTasks string variable back to an empty string
+    showTasks = "";
+  }
+  
+  // Checks if user entered NEW
+  if (userInput === `NEW`){
 
-        // Alerts the user that new task has been added
-        alert(`"${newTask}" has been added!`);
+    // Prompts the user to enter a new task and stores their response
+    newTask = prompt(`Please enter the new task:`);
 
-        // Adds the user's entry as a new item at the end of the new task array
-        tasks.push(newTask);
+    // Alerts the user that their new task has been added
+    alert(`"${newTask}" has been added!`);
+
+    // Adds the user's entry as a new item at the end of the tasks array
+    tasks.push(newTask);
+  }
+
+  // Checks if the user entered REMOVE
+  if (userInput === `REMOVE`){
+
+    // Concatenates each task/item in the tasks array to the showTasks string variable. Also, sets/displays a number for each task.
+    for(i = 0; i < tasks.length; i++){
+
+      // Adding 1 to i (AKA The index) so the number will start displaying at 1. Also, using \n to create a new line (AKA Line break)
+      showTasks += `${i + 1}: ${tasks[i]}\n`;
     }
 
-    // Displays the menu again
-    userInput = prompt(menu);
+    // Prompts the user to enter a number and stores their response to the num variable.
+    // Using \n to create a new line (AKA Line Break)
+    // Subtracting 1 fro  the user's entry so that is matches the index of the item that the user wants to remove from the tasks array.
+    // Just like with ParseInt, JavaScript will attempt to convert a string into a number when  you try to subtract from it. So ParserInt is not necessary here.
+    num = prompt(`Please enter a number to remove\n${showTasks}`) - 1;
+    
+    // Removes the task/item selected by the user from the task array. Also, set the task/item that was removed to the REMOVED variable (NOTE: Using index on the REMOVED variable because splice returns the value that is removed as an item in an array)
+    removed = tasks.splice(num, 1);
+
+    // Alerts use with the task/item that has been removed (NOTE: Using index on the REMOVED variable because splice returns the value that is removed as an item in an array)
+    alert(`"${removed[0]}" has been removed`);
+
+    // Sets the value of the the showTasks string variable back to an empty string
+    showTask = ``;
+  }
+
+  // Displays the menu again
+  userInput = prompt(menu);
 }
 
-//Alerts the user that they have close the program
-alert(`Thank you for using Task Manager!`);
+// Alerts the user that they have closed the program
+alert(`Thank you for using Task Manager`);
